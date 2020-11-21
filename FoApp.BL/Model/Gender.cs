5d234dee260fbc351a7aed0d16a45df4c69602c3
BARS\d.garipov <d.garipov@bars-open.ru>
2020-11-21 16:33:47 +0300

    Настройка проектов, модельки

---
+++ FoApp.BL/Model/Gender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FoApp.BL.Model
+{
+	/// <summary>
+	/// Пол
+	/// </summary>
+	public class Gender
+	{
+		/// <summary>
+		/// Название
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// СОздать пол
+		/// </summary>
+		/// <param name="name">имя поля</param>
+		public Gender(string name)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+			{
+				throw new ArgumentNullException("null или пусто", nameof(name));
+			}
+
+			Name = name;
+		}
+	}
+}
---
+++ FoApp.BL/Model/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoApp.BL.Model
+{
+	/// <summary>
+	/// Пользователь
+	/// </summary>
+	public class User
+	{
+		/// <summary>
+		/// Имя
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		/// Пол
+		/// </summary>
+		public Gender Gender { get; }
+
+		/// <summary>
+		/// Дата рождения
+		/// </summary>
+		public DateTime BirthDate { get; }
+
+		/// <summary>
+		/// Вес
+		/// </summary>
+		public double Weight { get; set; }
+
+		/// <summary>
+		/// Рост
+		/// </summary>
+		public double Height { get; set; }
+
+		/// <summary>
+		/// Создать нового пользователя
+		/// </summary>
+		/// <param name="name">Имя</param>
+		/// <param name="gender">Пол</param>
+		/// <param name="birthDate">Дата рождения</param>
+		/// <param name="weight">вес</param>
+		/// <param name="height">рост</param>
+		public User(string name, Gender gender, DateTime birthDate, double weight, double height)
+		{
+			#region Проверка условий
+			if (string.IsNullOrWhiteSpace(name))
+				throw new ArgumentNullException("Имя пользователя пусто или null", nameof(name));
+
+			if (gender == null)
+				throw new ArgumentNullException("Пол не может быть null", nameof(gender));
+
+			if (birthDate < DateTime.Parse("01.01.1900") && birthDate >= DateTime.Now)
+				throw new ArgumentNullException("дата рождения не соответствует", nameof(birthDate));
+
+			if (weight <= 0)
+				throw new ArgumentNullException("Вес не соответствует", nameof(weight));
+
+			if (height <= 0)
+				throw new ArgumentNullException("Рост рождения не соответствует", nameof(height));
+			#endregion
+
+			Name = name;
+			Gender = gender;
+			BirthDate = birthDate;
+			Weight = weight;
+			Height = height;
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
